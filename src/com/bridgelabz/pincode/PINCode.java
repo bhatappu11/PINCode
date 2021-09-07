2bfa1ecd62ece33cbafe63a7cb5aabaad43688d2
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-07 20:16:25 +0530

    MODIFIED: pincode with whitespace in the middle is considered valid

--- src/com/bridgelabz/pincode/PINCode.java
+++ src/com/bridgelabz/pincode/PINCode.java
@@ -8,7 +8,7 @@ public class PINCode {
 	public static void main(String[] args) {
 		Scanner scanner=new Scanner(System.in);
 		System.out.println("Enter the pin code");
-		String pinCode = scanner.next();
+		String pinCode = scanner.nextLine();
 		System.out.println("Is the pin code valid?");
 		if(checkForValidPinCode(pinCode))
 			System.out.println("It is valid");
@@ -18,7 +18,7 @@ public class PINCode {
 	}
 
 	private static boolean checkForValidPinCode(String pinCode) {
-		String regex = "^[0-9]{6}$";
+		String regex = "^[0-9]{3}[\\s]?[0-9]{3}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(pinCode);
 		return matcher.matches();
